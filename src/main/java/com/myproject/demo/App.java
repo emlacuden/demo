c212ae9a28846e3ed0fce8769429afb0dc5a92bf
emlacuden <hungpp250795@gmail.com>
2018-03-19 16:26:20 +0700

    first commit

---
+++ src/main/java/com/myproject/demo/App.java
@@ -0,0 +1,14 @@
+package com.myproject.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	String a ="assd";
+        System.out.println( "Hello World!" );
+    }
+}
